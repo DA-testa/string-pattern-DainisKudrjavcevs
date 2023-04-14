a22f2d746147e0158e879a5216f1f10350f28275
221RDB136 <125664089+DainisKudrjavcevs@users.noreply.github.com>
2023-04-14 12:08:11 +0000

    main

--- hash_substring.py
+++ hash_substring.py
@@ -1,29 +1,4 @@
-def print_occurrences(output):
-    print(' '.join(map(str, output)))
-
-def get_occurrences(pattern, text):
-    occurrences = []
-    p_len = len(pattern)
-    t_len = len(text)
-    prime = 1000000007
-    x = 263
-    if p_len > t_len:
-        return []
-
-    p_hash = sum(ord(pattern[i]) * pow(x, i, prime) for i in range(p_len)) % prime
-    t_hash = sum(ord(text[i]) * pow(x, i, prime) for i in range(p_len)) % prime
-    x_p = pow(x, p_len - 1, prime)
-
-    for i, char in enumerate(text):
-        if i >= p_len:
-            t_hash = ((t_hash - ord(text[i - p_len]) * x_p) * x + ord(char)) % prime
-        if p_hash == t_hash and pattern == text[i - p_len + 1:i + 1]:
-            occurrences.append(i - p_len + 1)
-
-    return occurrences
-
-
-def main():
+def read_input():
     input_method = input().strip()
 
     if input_method == "I":
@@ -50,6 +25,32 @@ def main():
             print_occurrences(occurrences)
         else:
             print("Invalid filename")
+        return pattern, text
+        
+
+def print_occurrences(output):
+    print(''.join(map(str, output)))
+
+def get_occurrences(pattern, text):
+    occurrences = []
+    p_len = len(pattern)
+    t_len = len(text)
+    prime = 1000000007
+    x = 263
+    if p_len > t_len:
+        return []
+
+    p_hash = sum(ord(pattern[i]) * pow(x, i, prime) for i in range(p_len)) % prime
+    t_hash = sum(ord(text[i]) * pow(x, i, prime) for i in range(p_len)) % prime
+    x_p = pow(x, p_len - 1, prime)
+
+    for i, char in enumerate(text):
+        if i >= p_len:
+            t_hash = ((t_hash - ord(text[i - p_len]) * x_p) * x + ord(char)) % prime
+        if p_hash == t_hash and pattern == text[i - p_len + 1:i + 1]:
+            occurrences.append(i - p_len + 1)
+
+    return occurrences
 
 if __name__ == '__main__':
-    main()
+    print_occurrences(get_occurrences(*read_input()))
