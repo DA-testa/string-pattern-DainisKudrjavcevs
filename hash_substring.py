f5e46acab4de032718bc811238b1973e86a64199
221RDB136 <125664089+DainisKudrjavcevs@users.noreply.github.com>
2023-04-14 12:28:31 +0000

    main

--- hash_substring.py
+++ hash_substring.py
@@ -1,54 +1,47 @@
 def read_input():
-    input_method = input().strip()
+    # Get the input type choice
+    input_type = input().rstrip()
 
-    if input_method == "I":
-        pattern = input().strip()
-        text = input().strip()
-        return pattern, text
+    # Read the pattern and text depending on the input type
+    if input_type == 'i':
+        pattern = input().rstrip()
+        text = input().rstrip()
+    else:
+        with open(input_type, 'r') as f:
+            pattern = f.readline().rstrip()
+            text = f.readline().rstrip()
 
-    elif input_method == "F":
-        filename = input().strip()
-        if str(filename[-1]) != "a":
-            try:
-                with open(f"./tests/{filename}") as f:
-                    contents = f.readlines()
-            except FileNotFoundError:
-                print("File not found")
-                return
-            except:
-                print("Error reading file")
-                return
-            pattern = contents[0].strip()
-            text = contents[1].strip()
-            return pattern, text
-        else:
-            print("Invalid filename")
-            return
+    return pattern, text
 
 
 def print_occurrences(output):
+    # Print the positions of the occurrences in the ascending order
     print(' '.join(map(str, output)))
 
+
 def get_occurrences(pattern, text):
-    occurrences = []
-    p_len = len(pattern)
-    t_len = len(text)
+    # Set the prime number and the base
     prime = 101
-    x = 256
-    if p_len > t_len:
-        return []
+    base = 256
 
-    p_hash = sum(ord(pattern[i]) * pow(x, i, prime) for i in range(p_len)) % prime
-    t_hash = sum(ord(text[i]) * pow(x, i, prime) for i in range(p_len)) % prime
-    x_p = pow(x, p_len - 1, prime)
+    # Calculate the hash value of the pattern and the first window of the text
+    pattern_hash = 0
+    text_hash = 0
+    for i in range(len(pattern)):
+        pattern_hash = (pattern_hash * base + ord(pattern[i])) % prime
+        text_hash = (text_hash * base + ord(text[i])) % prime
 
-    for i, char in enumerate(text):
-        if i >= p_len:
-            t_hash = ((t_hash - ord(text[i - p_len]) * x_p) * x + ord(char)) % prime
-        if p_hash == t_hash and pattern == text[i - p_len + 1:i + 1]:
-            occurrences.append(i - p_len + 1)
+    # Slide the window of the text over the pattern and compare the hash values
+    occurrences = []
+    for i in range(len(text) - len(pattern) + 1):
+        if pattern_hash == text_hash:
+            if pattern == text[i:i+len(pattern)]:
+                occurrences.append(i)
+        if i < len(text) - len(pattern):
+            text_hash = ((text_hash - ord(text[i]) * (base**(len(pattern)-1))) * base + ord(text[i+len(pattern)])) % prime
 
     return occurrences
 
+
 if __name__ == '__main__':
-    read_input()
+    print_occurrences(get_occurrences(*read_input()))
