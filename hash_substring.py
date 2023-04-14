70342f4cb15fd69601ec45a03ed12eb1cd9e744a
221RDB136 <125664089+DainisKudrjavcevs@users.noreply.github.com>
2023-04-14 12:55:21 +0000

    main

--- hash_substring.py
+++ hash_substring.py
@@ -1,8 +1,6 @@
 def read_input():
-    # Get the input type choice
     input_type = input().rstrip()
 
-    # Read the pattern and text depending on the input type
     if input_type == 'I':
         pattern = input().rstrip()
         text = input().rstrip()
@@ -27,23 +25,19 @@ def read_input():
 
 
 def print_occurrences(output):
-    # Print the positions of the occurrences in the ascending order
     print(' '.join(map(str, output)))
 
 
 def get_occurrences(pattern, text):
-    # Set the prime number and the base
     prime = 101
     base = 256
 
-    # Calculate the hash value of the pattern and the first window of the text
     pattern_hash = 0
     text_hash = 0
     for i in range(len(pattern)):
         pattern_hash = (pattern_hash * base + ord(pattern[i])) % prime
         text_hash = (text_hash * base + ord(text[i])) % prime
 
-    # Slide the window of the text over the pattern and compare the hash values
     occurrences = []
     for i in range(len(text) - len(pattern) + 1):
         if pattern_hash == text_hash:
